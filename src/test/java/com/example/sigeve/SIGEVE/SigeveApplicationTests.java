8db2867eefcb005d9db47902a9c88447f55d1f00
Israel Rios <77031754+JaderRios@users.noreply.github.com>
2025-11-01 18:49:30 -0500

    commit inicial

---
+++ src/test/java/com/example/sigeve/SIGEVE/SigeveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sigeve.SIGEVE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
